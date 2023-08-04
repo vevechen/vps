ed82671088c777a774fe680a2c1ca633d6276ed7
vevechen <107408964+vevechen@users.noreply.github.com>
2023-08-04 10:33:58 +0800

    Create CloudFlare_vlmess.cmd

---
+++ CloudFlare_vlmess.cmd
@@ -0,0 +1,50 @@
+部署代码：https://github.com/zizifn/edgetunnel/blob/main/src/worker-vless.js
+
+uuid生成：https://1024tools.com/uuid
+
+免费域名注册：https://www.dynadot.com/register-your-free-link-domain
+
+付费域名注册：www.namesilo.com（付费域名注册和解析点击查看这个视频>>跳转到4分17秒处）
+
+workers win专用ip优选：下载地址>>
+
+ip查看：https://whatismyipaddress.com/
+
+cf ip优选；https://stock.hostmonit.com/CloudFlareYes
+
+网络测速：https://www.speedtest.net/result/14952074175
+
+openclash转换订阅网址：https://sub-web.netlify.app/
+
+
+cdn加速:
+
+cdn-all.xn--b6gac.eu.org
+cdn.xn--b6gac.eu.org 
+cdn-b100.xn--b6gac.eu.org 
+edgetunnel.anycast.eu.org 
+cdn.anycast.eu.org （亚洲地区）
+
+1.有域名（有tls加密，443端口)，推荐！
+Custom Domains查看——添加自定义域——填1个二级域名
+点击自定义域，在二级域名后加/UUID，就能看到VLESS节点URL和Clash-meta配置
+在v2rayN导入URL，地址栏改为优选ip
+
+2.无域名（没tls加密，80或者2052端口）
+进去worker.dev，加上/uuid就能看到VLESS节点URL和Clash-meta配置。v2ray,shadowrocket等客户端要去掉tls加密，端口改为80或者2052，地址修为优选 ip
+
+
+
+cloudflare ip 优选
+https://github.com/XIU2/CloudflareSpeedTest/releases/download/v2.2.4/CloudflareST_darwin_amd64.zip
+
+ip 地址段：
+https://zip.baipiao.eu.org/
+
+
+10 个平均延迟低于 200 ms且下载速度高于3MB/s的IP
+
+
+sudo ./CloudflareST -url https://jp.cloudflarest.link -tl 200 -sl 3 -dn 10
+sudo ./CloudflareST -url https://hkcs.cloudflarest.link -tl 200 -sl 3 -dn 10
+
